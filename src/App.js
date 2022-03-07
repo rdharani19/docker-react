aa265013704aade36b564de7b400bbc018e3f08d
root <root@DC-PC.localdomain>
2022-03-07 17:53:03 -0500

    trigger rebuild

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React, now now now 
+          Learn React, now now now now now
         </a>
       </header>
     </div>
